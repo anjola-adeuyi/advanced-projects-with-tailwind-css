095473e599085a07986dcaf5d47cf06b69289872
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-15 04:04:30 +0100

    mobile nav javascript working

--- manage-landing-page/js/script.js
+++ manage-landing-page/js/script.js
@@ -3,4 +3,6 @@ const menu = document.getElementById('menu');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
 });
